d6bfb1a693b4b1d39ffe9ed6d616d99c214d4737
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-04-04 15:51:24 -0600

    Updated ABIs

--- frontend/src/constants/contracts.tsx
+++ frontend/src/constants/contracts.tsx
@@ -1,14 +1,16 @@
-import lotteryJson from '../assets/Lottery.json';
-import lotteryTokenJson from '../assets/LotteryToken.json';
+import govJson from '../assets/DiplomaGuildGov.json';
+import ftJson from '../assets/MarkingToken.json';
+import nftJson from '../assets/DiplomaGuildNFT.json';
+import tlJson from '../assets/DiplomaGuildTimeLock.json';
 
-const GOV_CONTRACT_ADDRESS = '0x4c5Ec2E9a3fC3eedE1DA4189B0f0db74eCeB7695';
-const FT_CONTRACT_ADDRESS = "0xca25D30F1f2f01Be696AFC5ADA4fbe9FFAea3755";
-const NFT_CONTRACT_ADDRESS = "0xca25D30F1f2f01Be696AFC5ADA4fbe9FFAea3755";
-const TL_CONTRACT_ADDRESS = "0xca25D30F1f2f01Be696AFC5ADA4fbe9FFAea3755";
+const GOV_CONTRACT_ADDRESS = '0xCb8551084133F0866baf730D276E7e09d70E1140';
+const FT_CONTRACT_ADDRESS = "0x66a2F4cdbB32475dFc8a8A631FCae05C8Eb0BaEc";
+const NFT_CONTRACT_ADDRESS = "0x68De8545D0f3c8cF41Ec820FDA5211f3072232e1";
+const TL_CONTRACT_ADDRESS = "0x5A3b5aB308d1aDAd46aD1b8D54f326752B4B5aAa";
 
-const GOV_ABI = lotteryJson.abi;
-const FT_ABI = lotteryTokenJson.abi;
-const NFT_ABI = lotteryJson.abi;
-const TL_ABI = lotteryJson.abi;
+const GOV_ABI = govJson.abi;
+const FT_ABI = ftJson.abi;
+const NFT_ABI = nftJson.abi;
+const TL_ABI = tlJson.abi;
 
-export { LOTTERY_CONTRACT_ADDRESS, LOTTERY_TOKEN_ADDRESS, LOTTERY_ABI, LOTTERY_TOKEN_ABI};
+export { GOV_CONTRACT_ADDRESS, FT_CONTRACT_ADDRESS, NFT_CONTRACT_ADDRESS, TL_CONTRACT_ADDRESS, GOV_ABI, FT_ABI, NFT_ABI, TL_ABI };
