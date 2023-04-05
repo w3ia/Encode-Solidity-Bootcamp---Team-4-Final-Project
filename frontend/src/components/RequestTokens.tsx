7030275513cc9bf7941952180744aa3613a8e3f8
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-04-05 14:09:02 -0600

    Reques tokens component working

--- frontend/src/components/RequestTokens.tsx
+++ frontend/src/components/RequestTokens.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { ethers } from 'ethers';
-import { useAccount, useContract, usePrepareContractWrite, useContractWrite } from 'wagmi';
+import { ethers, Signer } from 'ethers';
+import { useAccount, useContract, usePrepareContractWrite, useContractWrite, useSigner } from 'wagmi';
 import { FT_CONTRACT_ADDRESS, FT_ABI } from "../constants/contracts";
 
 export const RequestTokens = () => {
     const [amount, setAmount] = React.useState("0")
+    const [addr, setAddr] = React.useState("")
     const { address, isConnected, isDisconnected } = useAccount()
+    const { data: signer, isError } = useSigner()
     const formatEtherAmount = parseInt(amount) > 0 ? ethers.utils.parseEther(amount) : ethers.utils.parseEther("0");
-    const { config } = usePrepareContractWrite({
+
+    const ftC = useContract({
         address: FT_CONTRACT_ADDRESS,
         abi: FT_ABI,
-        functionName: 'mint',
-        args: [{value: formatEtherAmount}],
-    })
-    const { data, isLoading, isSuccess, write } = useContractWrite(config)
-      
-    const purchaseHandler = () => {
-        if (parseInt(amount) > 0) write?.();
-        else alert('Please enter an amount greater than 0')
+        signerOrProvider: signer,
+    });
+
+    async function submitHandler() {
+        if(ftC) {
+            const requestTokensTx = await ftC.mint(addr, formatEtherAmount)
+            const receiptTx = await requestTokensTx.wait()
+            console.log(`Tokens minted at block: ${receiptTx.blockNumber}`)
+        }
     }
     if (isConnected) {
         return (
@@ -27,35 +31,12 @@ export const RequestTokens = () => {
                     <h2 className="card-title">Request Voting Tokens Here</h2>
                     <p>Input the amount of tokens you want to request.</p>
                     <div className="card-actions justify-end">
-                    <input type="text" placeholder="Type here" className="input w-full max-w-xs" onChange={(e) => setAmount(e.target.value)} value={amount}/>
-                    <button className="btn btn-primary" onClick={purchaseHandler}>Buy Now</button>
-                    {isLoading && <div>Check Wallet</div>}
-                    {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+                    <input type="text" placeholder="Type here" className="input input-primary input-lg w-full max-w-xs bg-primary" onChange={(e) => setAmount(e.target.value)} value={amount}/>
+                    <input type="text" placeholder="Type here" className="input input-primary input-lg w-full max-w-xs bg-primary" onChange={(e) => setAddr(e.target.value)} value={addr}/>
+                    <button className="btn btn-primary" onClick={submitHandler}>Mint Now</button>
                     </div>
                 </div>
             </div>
-            // <Card sx={{ minWidth: 275, minHeight: 100 }}>
-            //     <CardContent>
-            //         <Typography component={'span'} variant={'body1'} align={'center'}>
-            //             <div>
-            //             <TextField
-            //                     id="amount"
-            //                     type="number"
-            //                     size="small"
-            //                     InputProps={{ inputProps: { min: 1 } }}
-            //                     onChange={(e) => setAmount(e.target.value)}
-            //                     placeholder="enter token amount"
-            //                     value={amount}
-            //                 />
-            //                 <Button variant="contained" disabled={!write} onClick={purchaseHandler}>
-            //                     Purchase Tokens
-            //                 </Button>
-            //                 {isLoading && <div>Check Wallet</div>}
-            //                 {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
-            //             </div>
-            //         </Typography>
-            //     </CardContent>
-            // </Card>
       );
     }
     return <div>Not Connected</div>
